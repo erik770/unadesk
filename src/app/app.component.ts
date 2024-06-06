c6ac0820b09dacf7f56c80ed1db09cb05783871b
Tadevosian Erik <e.tadevosian@norsi-trans.ru>
2024-06-06 20:25:13 +0300

    project init and configured

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,12 +1,13 @@
 import { Component } from '@angular/core';
 import { RouterOutlet } from '@angular/router';
+import { TuiRootModule } from '@taiga-ui/core';
 
 @Component({
   selector: 'app-root',
   standalone: true,
-  imports: [RouterOutlet],
+  imports: [RouterOutlet, TuiRootModule],
   templateUrl: './app.component.html',
-  styleUrl: './app.component.scss'
+  styleUrl: './app.component.scss',
 })
 export class AppComponent {
   title = 'unadesk-test';
---
+++ webpack.ext.config.js
@@ -0,0 +1,15 @@
+/*eslint-env es6*/
+const ESLintPlugin = require("eslint-webpack-plugin");
+
+module.exports = {
+  plugins: [
+    new ESLintPlugin({
+      extensions: ["ts", "html"],
+      lintDirtyModulesOnly: false,
+      emitWarning: true,
+      emitError: true,
+      failOnWarning: true,
+      failOnError: true,
+    }),
+  ],
+};
